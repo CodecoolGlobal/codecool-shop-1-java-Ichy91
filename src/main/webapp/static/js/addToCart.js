d0e2203cf259fa2d26095088914eeaab0ed16fb7
Stark Máté <strkmt@gmail.com>
2021-09-01 14:21:27 +0200

    feat: add addToCart.js

--- src/main/webapp/static/js/addToCart.js
+++ src/main/webapp/static/js/addToCart.js
@@ -1,17 +1,26 @@
 window.onload = function() {
+
+    function putData(url) {
+    }
+
     function addToCart() {
         let numberOfItems = 0
         let cart = document.getElementById("cart")
         cart.innerHTML = "Cart: " + numberOfItems + " item(s)"
         let addToCartButtons = document.getElementsByClassName("btn btn-success")
         for (let button of addToCartButtons) {
-            button.addEventListener("click", function() {
+            button.addEventListener("click", async function() {
                 numberOfItems += 1
                 cart.innerHTML = "Cart: " + numberOfItems + " item(s)"
+                let id = button.dataset.id
+                let url = "/cart?id=" + id
+                await putData(url)
             })
         }
 
     }
+
+
 
     addToCart()
 }
